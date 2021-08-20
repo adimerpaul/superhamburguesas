36a6f18cb0cf92cebd8efdf787130c42d5695c32
adimerpaul <adimer101@email.com>
2021-08-19 21:22:19 -0400

    mitad login de super hambugesas

--- superfront/src/pages/Login.vue
+++ superfront/src/pages/Login.vue
@@ -87,14 +87,82 @@
                     </q-input>
                   </div>
                   <div class="col-12">
-                    <l-map ref="map" style="height:50vh" v-model:zoom="zoom" :center="[44.48865, 11.3317]">
-                      <l-tile-layer
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        layer-type="base"
-                        name="OpenStreetMap"
-                      ></l-tile-layer>
-                      <l-circle :lat-lng="[44.48865, 11.3317]" :radius="5000" color="green" />
-                    </l-map>
+                    <div style="height: 50vh;">
+                      <l-map
+                        @ready="onReady"
+                        @locationfound="onLocationFound"
+                        v-model="zoom"
+                        v-model:zoom="zoom"
+                        :center="center"
+                        @click="ubicacion"
+
+                      >
+<!--                        @move="log('move')"-->
+                        <l-tile-layer
+                          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        ></l-tile-layer>
+                        <l-control-layers />
+<!--                        <l-marker :lat-lng="[0, 0]" draggable @moveend="log('moveend')">-->
+<!--                          <l-tooltip>-->
+<!--                            lol-->
+<!--                          </l-tooltip>-->
+<!--                        </l-marker>-->
+
+<!--                        <l-marker :lat-lng="[47.41322, -1.219482]">-->
+<!--                          <l-icon :icon-url="iconUrl" :icon-size="iconSize" />-->
+<!--                        </l-marker>-->
+
+                        <l-marker :lat-lng="center" draggable @moveend="log">
+                          <l-popup>
+                            lol
+                          </l-popup>
+                        </l-marker>
+
+<!--                        <l-polyline-->
+<!--                          :lat-lngs="[-->
+<!--          [47.334852, -1.509485],-->
+<!--          [47.342596, -1.328731],-->
+<!--          [47.241487, -1.190568],-->
+<!--          [47.234787, -1.358337],-->
+<!--        ]"-->
+<!--                          color="green"-->
+<!--                        ></l-polyline>-->
+<!--                        <l-polygon-->
+<!--                          :lat-lngs="[-->
+<!--          [46.334852, -1.509485],-->
+<!--          [46.342596, -1.328731],-->
+<!--          [46.241487, -1.190568],-->
+<!--          [46.234787, -1.358337],-->
+<!--        ]"-->
+<!--                          color="#41b782"-->
+<!--                          :fill="true"-->
+<!--                          :fillOpacity="0.5"-->
+<!--                          fillColor="#41b782"-->
+<!--                        />-->
+<!--                        <l-rectangle-->
+<!--                          :lat-lngs="[-->
+<!--          [46.334852, -1.509485],-->
+<!--          [46.342596, -1.328731],-->
+<!--          [46.241487, -1.190568],-->
+<!--          [46.234787, -1.358337],-->
+<!--        ]"-->
+<!--                          :fill="true"-->
+<!--                          color="#35495d"-->
+<!--                        />-->
+<!--                        <l-rectangle-->
+<!--                          :bounds="[-->
+<!--          [46.334852, -1.190568],-->
+<!--          [46.241487, -1.090357],-->
+<!--        ]"-->
+<!--                        >-->
+<!--                          <l-popup>-->
+<!--                            lol-->
+<!--                          </l-popup>-->
+<!--                        </l-rectangle>-->
+                      </l-map>
+<!--                      <button @click.prevent="changeIcon">New kitten icon</button>-->
+
+                    </div>
                   </div>
                   <div class="col-12">
                     <q-input outlined v-model="user.password" label="Password*" :type="isPwd ? 'password' : 'text'" hint="Porfavor ingresar contraseña">
@@ -116,7 +184,6 @@
                     <div class="text-caption q-py-xs">
                       <a href="#">Olvidate tu contraseña?</a>
                     </div>
-
                   </div>
                 </div>
               </q-form>
@@ -221,28 +288,40 @@
 // import { LMap, LTileLayer } from 'vue2-leaflet'
 // import L from 'leaflet'
 // import 'leaflet/dist/leaflet.css'
-import "leaflet/dist/leaflet.css"
-import { LMap, LTileLayer, LCircle }from "@vue-leaflet/vue-leaflet";
+import {
+  LMap,
+  LIcon,
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+  LTooltip,
+  LPopup,
+  LPolyline,
+  LPolygon,
+  LRectangle,
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default {
   name: 'MainLayout',
   components: {
     LMap,
+    // LIcon,
     LTileLayer,
-    LCircle,
+    LMarker,
+    LControlLayers,
+    // LTooltip,
+    LPopup,
+    // LPolyline,
+    // LPolygon,
+    // LRectangle,
   },
   data(){
     return{
-      geojson: {
-        type: "FeatureCollection",
-        features: [
-          // ...
-        ],
-      },
-      geojsonOptions: {
-        // Options that don't rely on Leaflet methods.
-      },
-      zoom:11,
+      zoom: 13,
+      center:[-17.970371, -67.112303],
+      iconWidth: 25,
+      iconHeight: 40,
       celular:'69603027',
       password:'admin',
       tab:'registro',
@@ -250,18 +329,46 @@ export default {
       isPwd:true,
     }
   },
-  async beforeMount() {
-    // HERE is where to load Leaflet components!
-    const { circleMarker } = await import("leaflet/dist/leaflet-src.esm");
-
-    // And now the Leaflet circleMarker function can be used by the options:
-    this.geojsonOptions.pointToLayer = (feature, latLng) =>
-      circleMarker(latLng, { radius: 8 });
-    this.mapIsReady = true;
+  // async beforeMount() {
+  //   // HERE is where to load Leaflet components!
+  //   const { circleMarker } = await import("leaflet/dist/leaflet-src.esm");
+  //
+  //   // And now the Leaflet circleMarker function can be used by the options:
+  //   this.geojsonOptions.pointToLayer = (feature, latLng) =>
+  //     circleMarker(latLng, { radius: 8 });
+  //   this.mapIsReady = true;
+  // },
+  computed: {
+    iconUrl() {
+      return `https://placekitten.com/${this.iconWidth}/${this.iconHeight}`;
+    },
+    iconSize() {
+      return [this.iconWidth, this.iconHeight];
+    },
   },
-  mounted() {
-    // console.log(this.center)
-  }
+  methods: {
+    onReady (mapObject) {
+      mapObject.locate();
+    },
+    onLocationFound(location){
+      console.log(location)
+      let radius = location.accuracy / 2
+      console.log( radius	)
+      this.center=[location.latlng.lat,location.latlng.lng]
+    },
+    log(a) {
+      // console.log(a.target._lastCenter.lat)
+      console.log(a)
+      // console.log(a.target._lastCenter.lat);
+      // console.log(this.center)
+    },
+    changeIcon() {
+      this.iconWidth += 2;
+      if (this.iconWidth > this.iconHeight) {
+        this.iconWidth = Math.floor(this.iconHeight / 2);
+      }
+    },
+  },
   // setup () {
   //   const $store = useStore()
   //   const $q = useQuasar()
