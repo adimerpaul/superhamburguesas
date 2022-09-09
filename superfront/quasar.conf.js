c23d5d6250ccc27900bbc424165b7f0f2cd383ae
adimerpaul <adimer101@gmail.com>
2022-09-09 09:54:33 -0400

    colocado el pwa en local

--- superfront/quasar.conf.js
+++ superfront/quasar.conf.js
@@ -152,7 +152,7 @@ module.exports = configure(function (ctx) {
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#e30202',
         icons: [
           {
             src: 'icons/icon-128x128.png',
